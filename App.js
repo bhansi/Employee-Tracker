8233c51fe77fd1db16810dcc3292bff307e29bd0
Baljot Hansi <baljotshansi@gmail.com>
2023-10-15 15:07:23 -0300

    Added logic to connect to the business_db database.

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DATABASE_PASSWORD,
+        database: 'business_db'
+    },
+    console.info('Successfully connected to business_db database.')
+);
