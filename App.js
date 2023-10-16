aa3ffe6cb84c96cf25767b92602c5e6514949528
Baljot Hansi <baljotshansi@gmail.com>
2023-10-16 10:32:18 -0300

    Added logic to ask user what action they would like to perform.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const QueryMaker = require('./lib/query.js')
 require('dotenv').config();
 
 const db = mysql.createConnection(
@@ -9,5 +10,74 @@ const db = mysql.createConnection(
         password: process.env.DATABASE_PASSWORD,
         database: 'business_db'
     },
-    console.info('Successfully connected to business_db database.')
+    console.info('Successfully connected to business_db database.\n')
 );
+
+function inquireCommand(command) {
+    let message = `${command.split(' ')[0]} ${command.split(' ')[1].toLowerCase()}`;
+    command = command.split(' ')[0];
+
+    switch(command) {
+        case 'View':
+        case 'Update':
+        case 'Delete':
+            message += ' from:';
+            break;
+        case 'Add':
+            message += ' to:';
+            break;
+    }
+
+    const tables = [
+        'Department',
+        'Role',
+        'Employee'
+    ];
+
+    let question = {
+        type: 'list',
+        name: 'table',
+        message: message,
+        choices: tables
+    }
+
+    inquirer
+        .prompt(question)
+        .then((response) => {
+            let { table } = response;
+            console.log(response);
+            db.execute()
+        });
+}
+
+function inquire() {
+    const commands = [
+        'View Records',
+        'Add Record',
+        'Update Record',
+        'Delete Record',
+        'Quit'
+    ];
+
+    const question = [
+        {
+            type: 'list',
+            name: 'command_type',
+            message: 'Which action would you like to perform?',
+            choices: commands
+        }
+    ];
+
+    inquirer
+        .prompt(question)
+        .then((response) => {
+            let { command } = response;
+            commands === 'Quit' ? process.exit() : inquireCommand(command);
+        });
+}
+
+function init() {
+    inquire();
+}
+
+init();
